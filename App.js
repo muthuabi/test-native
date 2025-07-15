433c8a659d178ce5b12cce1ac61d1cd9862120dd
Muthukrishnan M <muthuabi292@gmail.com>
2025-07-15 22:45:20 +0530

    Initial Base Commit on Master

--- App.js
+++ App.js
@@ -4,12 +4,11 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Master Base Code... Checkout a Branch with Exp/App Name and use it</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
