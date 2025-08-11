888d3fd5494bb43c6f602dfc999c429e93fa47fd
Muthukrishnan M <muthuabi292@gmail.com>
2025-08-11 21:57:19 +0530

    Started

--- App.js
+++ App.js
@@ -1,12 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import Clock from 'react-clock';
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello</Text>
-      <Clock />
-      <StatusBar style="auto" />
+      
     </View>
   );
 }
