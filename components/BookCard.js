97999edf9f540551b3e9a8caf7c020bccf25e560
Muthukrishnan M <muthuabi292@gmail.com>
2025-08-13 23:01:03 +0530

    Initial Setup

---
+++ components/BookCard.js
@@ -0,0 +1,12 @@
+import React,{useState,useEffect} from 'react';
+import {View,Text} from 'react-native';
+import {Modal} from 'react-native-modal';
+import {Card} from "react-native-paper";
+
+export default function BookCard()
+{
+    return(
+    <Card>
+        
+    </Card>);
+}
---
+++ pages/Home.js
@@ -0,0 +1,9 @@
+import {View,StyleSheet} from 'react-native';
+export default function Home()
+{
+    return(
+        <View>
+            
+        </View>
+    );
+}
