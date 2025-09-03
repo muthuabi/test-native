8fb23fe03d0257e0c84d20d85d000ed2cecb0fb4
Muthukrishnan M <muthuabi292@gmail.com>
2025-09-04 01:04:06 +0530

    Ex5 Icon imported

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { Provider as PaperProvider } from 'react-native-paper';
+import { Provider as PaperProvider,Icon } from 'react-native-paper';
 import Home from './pages/Home';
 import Scanner from './pages/Scanner';
 
