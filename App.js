97320af92f92b376170011514a21ccf762692090
Muthukrishnan M <muthuabi292@gmail.com>
2025-10-14 00:44:24 +0530

    Intial

--- App.js
+++ App.js
@@ -1,19 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-
+import SinglePageAttendance from './screens/SinglePageAttendance';
+import { PaperProvider,MD2DarkTheme as dark,MD2LightTheme as light } from 'react-native-paper';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 export default function App() {
+  const Stack=createNativeStackNavigator();
   return (
+    <PaperProvider theme={light}>
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <NavigationContainer>
+          <Stack.Navigator>
+            <Stack.Screen name="Attendance" component={SinglePageAttendance} />
+          </Stack.Navigator>
+      </NavigationContainer>
     </View>
+    </PaperProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
---
+++ screens/SinglePageAttendance.js
@@ -0,0 +1,20 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import {View,Stylesheet,Text,FlatList} from 'react-native';
+import {Card,Switch} from 'react-native';
+
+export default function SinglePageAttendance({navigation})
+{
+    return(<View>
+
+    </View>);
+}
+
+const styles=Stylesheet.create({
+    container:{
+        padding:5,
+    },
+    boldText:
+    {
+
+    }
+});
